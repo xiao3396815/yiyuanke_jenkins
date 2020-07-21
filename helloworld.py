4d3075478fd22e844f7874ae15dbf93187a96cc6
xiao3396815 <592264245@qq.com>
2020-07-21 20:21:04 +0800

    项目新增0721

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,17 @@
+from cmath import polar
+
 print("hello world!")
 print("你好")
 print("世界")
 print("世界")
-prinlll222()
+
+
+# 极坐标图形  ---磨具
+from pyecharts import Polar
+
+radius =['周一','周二','周三','周四','周五,'周六','周日']
+
+polar = polar('极坐标')
+
+# 添加
+polar.add
